84838426ecc0a9f2b5e691b1dcd95d4d3e510a7c
Paul Milbank <paulmilbank@gmail.com>
2017-07-28 20:17:49 +1200

    hello world

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express')
 const settings = require('./settings/settings.json')
+const express = require('express')
 
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(settings.port)
