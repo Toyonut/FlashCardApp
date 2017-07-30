8f47f148220bed53c3c4731a315bea55e0765edd
Paul Milbank <paulmilbank@gmail.com>
2017-07-30 20:52:01 +1200

    change to pug and update to render method.

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const settings = require('./settings/settings.json')
 const express = require('express')
 
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'purple'
+]
+
+const names = [
+  {first: 'Bill',
+    last: 'Gates'},
+  {first: 'Ted',
+    last: 'Bundy'},
+  {first: 'Bill',
+    last: 'Nye'},
+  {first: 'William',
+    last: 'Shatner'},
+  {first: 'Spider',
+    last: 'man'}
+]
+
 const app = express()
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.render('index', {colors})
 })
 
-app.get('/about', (req, res) => {
-  res.send('<h3>Made with love by Paul</h3>')
+app.get('/card', (req, res) => {
+  res.render('card', {
+    prompt: 'who is buried in Grants tomb?',
+    hint: 'Think about whose tomb it is.'
+  })
+})
+
+app.get('/sandbox', (req, res) => {
+  res.render('sandbox', {names})
 })
 
 app.listen(settings.port, () => {
