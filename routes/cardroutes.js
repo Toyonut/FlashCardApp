49c5387d96371bdbdc02eee2ba072b6ffe036aaa
Paul Milbank <paulmilbank@gmail.com>
2017-08-10 20:59:51 +1200

    comment for concept I just understood.

--- routes/cardroutes.js
+++ routes/cardroutes.js
@@ -7,7 +7,7 @@ router.get('/:id', (req, res) => {
   const name = req.cookies.username
   const { id } = req.params
   const { side } = req.query
-  const text = cards[id][side]
+  const text = cards[id][side] // this combines the id number for the Q with the side (question or answer). depending on side will set value of text.
   const { hint } = cards[id]
   const templateData = { text }
 
